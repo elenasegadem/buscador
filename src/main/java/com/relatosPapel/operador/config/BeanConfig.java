0a344b925eed43b6e6e28e2cb5eb9ef4c06e10c0
elenasm <elena.segademar0102@comunidadunir.net>
2026-02-04 16:46:54 +0100

    H2 y eureka added

--- src/main/java/com/relatosPapel/operador/config/BeanConfig.java
+++ src/main/java/com/relatosPapel/operador/config/BeanConfig.java
@@ -2,7 +2,7 @@ package com.relatosPapel.operador.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 public class BeanConfig {
