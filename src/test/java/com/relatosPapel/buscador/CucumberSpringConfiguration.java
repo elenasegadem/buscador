9833ef9c069443f6609415501e23008f1beaaab0
elenasm <elena.segademar0102@comunidadunir.net>
2026-02-22 16:32:12 +0100

    Tests reverted

--- src/test/java/com/relatosPapel/buscador/CucumberSpringConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.relatosPapel.buscador;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = BuscadorApplication.class)
-public class CucumberSpringConfiguration {
-}
