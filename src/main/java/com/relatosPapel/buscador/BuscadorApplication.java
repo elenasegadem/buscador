42768e0e3f1f77d5d08a984b39d823e95439bbd0
elenasm <elena.segademar0102@comunidadunir.net>
2026-02-26 18:47:17 +0100

    Profile added

--- src/main/java/com/relatosPapel/buscador/BuscadorApplication.java
+++ src/main/java/com/relatosPapel/buscador/BuscadorApplication.java
@@ -8,6 +8,15 @@ public class BuscadorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BuscadorApplication.class, args);
+		String profile = System.getenv("PROFILE");
+		System.setProperty("spring.profiles.active", profile != null ? profile : "default");
+		// Railway's internal interface takes some time to start. We wait for it to be ready.
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+		SpringApplication.run(BuscadorApplication.class, args);
 	}
 
 }
