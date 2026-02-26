8bcc3ad7a60cfb72a3c32be603ad39a362aedcb8
elenasm <elena.segademar0102@comunidadunir.net>
2026-02-26 18:52:22 +0100

    Profile deleted

--- src/main/java/com/relatosPapel/buscador/BuscadorApplication.java
+++ src/main/java/com/relatosPapel/buscador/BuscadorApplication.java
@@ -8,14 +8,6 @@ public class BuscadorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BuscadorApplication.class, args);
-		String profile = System.getenv("PROFILE");
-		System.setProperty("spring.profiles.active", profile != null ? profile : "default");
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-		}
-		SpringApplication.run(BuscadorApplication.class, args);
 	}
 
 }
